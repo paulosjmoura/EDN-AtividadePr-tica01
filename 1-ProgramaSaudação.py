b19393ced45cbdc0cd8d4dedb29391be5a1a2fe9
paulosjmoura <paulosjmoura@gmail.com>
2025-07-27 19:51:31 -0300

    Add files via upload

---
+++ 1-ProgramaSaudação.py
@@ -0,0 +1,3 @@
+#1- Programa de Saudação
+#* Crie um programa que imprima a mensagem "Olá, mundo!" na tela.
+print("Olá, mundo!")
---
+++ 4-CalculadoraPreçoTotal.py
@@ -0,0 +1,17 @@
+'''4- Calculadora de Preço Total
+* Desenvolva um programa que calcula o preço total de uma compra. Use as seguintes informações:
+
+* Nome do produto: "Cadeira Infantil"
+* Preço unitário: R$ 12.40
+* Quantidade: 3
+O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final.'''
+nomeProduto = "Cadeira Infantil"
+preçoUnitário = 12.40
+quantidade = 3
+total = preçoUnitário * quantidade
+precoTotal =  f"Valor total a ser pago em {quantidade} unidade(s) é de R$ {total}"
+
+
+print(nomeProduto)
+print(precoTotal)
+
